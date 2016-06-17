5fcd9bae71d9346b42083c99f155bf2a2eb8269e
模范青蛙 <shc@mofanqingwadeMacBook-Pro.local>
2016-06-17 17:10:03 +0800

    spring中配置文件测试-shc

---
+++ src/test/java/com/shc/spring/service/impl/StoneAxe.java
@@ -0,0 +1,20 @@
+package com.shc.spring.service.impl;
+
+import com.shc.spring.service.*;
+/**
+ * Description:
+ * <br/>网站: <a href="http://www.crazyit.org">疯狂Java联盟</a>
+ * <br/>Copyright (C), 2001-2016, Yeeku.H.Lee
+ * <br/>This program is protected by copyright laws.
+ * <br/>Program Name:
+ * <br/>Date:
+ * @author  Yeeku.H.Lee kongyeeku@163.com
+ * @version  1.0
+ */
+public class StoneAxe implements Axe
+{
+	public String chop()
+	{
+		return "石斧砍柴好慢";
+	}
+}
